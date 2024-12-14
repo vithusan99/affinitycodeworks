0acd2d103d5e122359d15748378ca846bc9563e4
vithsan99 <vithusan091999@gmail.com>
2024-12-14 20:40:49 +0530

    ui design updated

--- data.js
+++ data.js
@@ -2,35 +2,35 @@ const projects = [
     {
         title: 'YarlMakket - E Commerce',
         description: 'A modern e-commerce solution with real-time inventory management and secure payment processing.',
-        image: '/images/projects/yarl-makket.jpg',
+        image: 'images/projects/yarl-makket.jpg',
         site: 'https://yarlmakket.herokuapp.com/',
         tech: ['React', 'JAVA', 'MongoDB']
     },
     {
         title: 'Task Management System',
         description: 'Collaborative project management tool with real-time updates and team communication features.',
-        image: '/images/projects/p1.png',
+        image: 'images/projects/p1.png',
         site: 'https://task.vintageinterior.co.uk/dashboard',
         tech: ['Vue.js', 'Laravel', 'MySQL']
     },
     {
         title: 'MaxCare - E Medical Platform',
         description: 'Data visualization platform providing real-time insights and customizable reporting.',
-        image: '/images/projects/emedic.png',
+        image: 'images/projects/emedic.png',
         site: 'http://pirunthapan.rabatguru.dk',
         tech: ['Wordpress', 'Elementor', 'MySQL']
     },
     {
         title: 'Job Search Dashboard',
         description: 'Data visualization platform providing real-time insights and customizable reporting.',
-        image: '/images/projects/p2.png',
+        image: 'images/projects/p2.png',
         site: 'https://vithusan99.github.io/JobFinderWebApp/',
         tech: ['React', 'Tailwind CSS']
     },
     {
         title: 'Greemy - Flower Selling Platform',
         description: 'Data visualization platform providing real-time insights and customizable reporting.',
-        image: '/images/projects/p10.png',
+        image: 'images/projects/p10.png',
         site: '',
         tech: ['HTML', 'CSS']
     },
@@ -38,7 +38,7 @@ const projects = [
     {
         title: 'HooBank - Payment Potral',
         description: 'Data visualization platform providing real-time insights and customizable reporting.',
-        image: '/images/projects/p3.png',
+        image: 'images/projects/p3.png',
         site: 'https://vithusan99.github.io/Vite_react_Bank_App/',
         tech: ['React', 'Tailwind CSS']
     },
@@ -46,7 +46,7 @@ const projects = [
     {
         title: 'Airbnb - Cloned',
         description: 'Clone of AirBnb website',
-        image: '/images/projects/p5.png',
+        image: 'images/projects/p5.png',
         site: 'https://sooriyakumar23.github.io/Airbnb_Static/',
         tech: ['HTML', 'CSS']
     }
@@ -56,7 +56,7 @@ const team = [
     {
         name: 'Pirunthapan Murugaiah',
         role: 'Software Engineer',
-        image: 'images/staffs/pirun3.jpg',
+        image: 'images/staffs/pirun5.jpg',
         bio: 'Software Engineer Passionate about transforming innovative ideas into functional and efficient software solutions.',
         technology: 'Springboot, React, MongoDB, MySQL, Java',
         github: 'https://github.com/mpirunthapan',
@@ -74,7 +74,7 @@ const team = [
     {
         name: 'Jesumani Anithvithushan',
         role: 'Full-stack developer',
-        image: 'images/anith2.JPG',
+        image: 'images/staffs/anith5.JPG',
         bio: 'Full-stack developer with 3+ years of experience in building scalable web applications.',
         technology: 'Java, React, Angular, Vue JS, Laravel, MongoDB, MySQL, Figma',
         github: 'https://github.com/vithusan99',
--- main.js
+++ main.js
@@ -1,84 +1,94 @@
 // Mobile Navigation
-const menuToggle = document.getElementById('menuToggle');
-const navLinks = document.getElementById('navLinks');
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    const icon = menuToggle.querySelector('i');
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-times');
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  const icon = menuToggle.querySelector("i");
+  icon.classList.toggle("fa-bars");
+  icon.classList.toggle("fa-times");
 });
 
-function truncateText(text){
-    console.log('truncate text',text);
-    return text?.length>60 ? text.slice(0,60)+'...' : text;
+function truncateText(text) {
+  console.log("truncate text", text);
+  return text?.length > 60 ? text.slice(0, 60) + "..." : text;
 }
 // Projects Section
-const projectsGrid = document.getElementById('projectsGrid');
+const projectsGrid = document.getElementById("projectsGrid");
 
-projects.forEach(project => {
-    const projectCard = document.createElement('div');
-    projectCard.className = 'project-card';
-    projectCard.innerHTML = `
+projects.forEach((project) => {
+  const projectCard = document.createElement("div");
+  projectCard.className = "project-card";
+  projectCard.innerHTML = `
         <img src="${project.image}" alt="${project.title}">
         <div class="project-content">
             <h3>${project.title}</h3>
             <p>${truncateText(project.description)}</p>
             <div class="tech-tags">
-                ${project.tech.map(tech => `
+                ${project.tech
+                  .map(
+                    (tech) => `
                     <span class="tech-tag">
                         <i class="fas fa-code"></i>
                         ${tech}
                     </span>
-                `).join('')}
+                `
+                  )
+                  .join("")}
             </div>
-            <button class="cta-button">
-                View Details
-                <i class="fas fa-external-link-alt"></i>
-            </button>
+            <button class="contactButton cursor-hand" type="button" id="cta-button" onclick="openLink('${project.site}')"> View
+  <div class="iconButton">
+    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+  </div>
+</button>
         </div>
     `;
-    projectsGrid.appendChild(projectCard);
+  projectsGrid.appendChild(projectCard);
 });
 
+function openLink(url) {
+    console.log('url',url);
+    
+    window.open(url, '_blank'); // Opens the link in a new tab
+}
 // Team Section
-const teamGrid = document.getElementById('teamGrid');
+const teamGrid = document.getElementById("teamGrid");
 
-team.forEach(member => {
-    const teamCard = document.createElement('div');
-    teamCard.className = 'team-card';
-    teamCard.innerHTML = `
+team.forEach((member) => {
+  const teamCard = document.createElement("div");
+  teamCard.className = "team-card";
+  teamCard.innerHTML = `
         <img src="${member.image}" alt="${member.name}">
         <div class="team-content">
             <h3>${member.name}</h3>
             <p class="role">${member.role}</p>
             <p>${member.bio}</p>
             <div class="social-links">
-                <a href="${member.github}"><i class="fab fa-github"></i></a>
-                <a href="${member.linkedin}"><i class="fab fa-linkedin"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="${member.github}"  target="_blank"><i class="fab fa-github"></i></a>
+                <a href="${member.linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-twitter"></i></a>
             </div>
         </div>
     `;
-    teamGrid.appendChild(teamCard);
+  teamGrid.appendChild(teamCard);
 });
 
 // Contact Form
-const contactForm = document.getElementById('contactForm');
+const contactForm = document.getElementById("contactForm");
 
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Here you would typically handle the form submission
-    alert('Thank you for your message! We will get back to you soon.');
-    contactForm.reset();
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Here you would typically handle the form submission
+  alert("Thank you for your message! We will get back to you soon.");
+  contactForm.reset();
 });
 
 // Back to Top
-const backToTop = document.getElementById('backToTop');
+const backToTop = document.getElementById("backToTop");
 
-backToTop.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
 // Update Copyright Year
-document.getElementById('currentYear').textContent = new Date().getFullYear();
+document.getElementById("currentYear").textContent = new Date().getFullYear();
